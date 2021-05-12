dbb034d57d7b18fb0916115ed496310740912800
Wogified-Main <8029325+Wogified@users.noreply.github.com>
2021-05-12 17:34:25 -0400

    Initial commit

---
+++ core/src/main/java/woga/boga/Game.java
@@ -0,0 +1,19 @@
+package woga.boga;
+
+public interface Game {
+    int getNumber();
+
+    int getGuess();
+
+    void setGuess(int guess);
+
+    int getSmallest();
+    int getBiggest();
+    int getRemainingGuesses();
+    void reset();
+    void check();
+    boolean isValidNumberRange();
+    boolean isGameWon();
+    boolean isGameLost();
+
+}
---
+++ core/src/main/java/woga/boga/NumberGenerator.java
@@ -0,0 +1,6 @@
+package woga.boga;
+
+public interface NumberGenerator {
+    int next();
+    int getMaxNumber();
+}
